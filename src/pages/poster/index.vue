7f8b455f974cf5e5f0591949a5e7ab7dc8670be9
ryan <ryan@ryandeMacBook-Pro.local>
2019-05-15 01:47:33 +0800

    初始化poster页面

--- src/pages/poster/index.vue
+++ src/pages/poster/index.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>Poster</div>
+    <div class="jike-poster">
+        <canvas canvas-id='poster' style='width:100vw;height:90vh;'></canvas>
+        <button class='save-btn' @click="saveImage">保存到相册</button>
+    </div>
 </template>
 
 <script>
@@ -7,20 +10,45 @@
 export default {
     data() {
         return {
-
+            currentCond: {}
         };
     },
-
-    components: {},
-
     methods: {
+        drawImage() {
+            let context = wx.createCanvasContext('poster');
+            context.setStrokeStyle('#00ff00');
+            context.setLineWidth(5);
+            context.rect(0, 0, 200, 200);
+            context.stroke();
+            context.draw(false, this.getTempFilePath());
+        },
+        getTempFilePath() {
+            wx.canvasToTempFilePath({
+                canvasId: 'share',
+                success: (res) => {
+                    this.shareTempFilePath = res.tempFilePath;
+                }
+            });
+        },
+        saveImage() {
+            wx.saveImageToPhotosAlbum({
+                filePath: this.shareTempFilePath,
+                success: res => {
+                    console.log(res);
+                },
+                fail: err => {
+                    console.log(err);
+                }
+            });
+        }
     },
-
-    mounted() {
+    onReady() {
+        this.currentCond = wx.getStorageSync('currentCond') || {};
+        console.log('this.currentCond', this.currentCond);
+        this.drawImage();
     }
 };
 </script>
 
 <style lang="scss" scoped>
-
 </style>
